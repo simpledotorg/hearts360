b8921f57df50514939c6be29d82aa5cab0ee7f9c
Daniel Burka <danielburka@gmail.com>
2023-08-27 19:43:28 +0100

    Finished adding cohors and comorbid control data

--- js/charts.js
+++ js/charts.js
@@ -964,3 +964,57 @@ const dmStatinsCanvas = document.getElementById("dmstatins");
 if (dmStatinsCanvas) {
   createChart(dmStatinsCanvas, dmStatinsConfig);
 }
+
+// Co-morbid Hypertension and Diabetes: BP Uncontrolled
+const comorbidControlledData = {
+  labels: [
+    "Mar-2022",
+    "Apr-2022",
+    "May-2022",
+    "Jun-2022",
+    "Jul-2022",
+    "Aug-2022",
+    "Sep-2022",
+    "Oct-2022",
+    "Nov-2022",
+    "Dec-2022",
+    "Jan-2023",
+    "Feb-2023",
+    "Mar-2023",
+    "Apr-2023",
+    "May-2023",
+    "Jun-2023",
+    "Jul-2023",
+    "Aug-2023",
+  ],
+  datasets: [
+    {
+      label: "BP controlled",
+      data: [
+        26, 28, 31, 30, 30, 32, 36, 38, 39, 41, 40, 42, 45, 44, 45, 47, 50, 48,
+      ],
+      borderColor: "#3BB231",
+      backgroundColor: "rgba(69, 205, 57, 0.1)",
+      segment: {
+        borderDash: (ctx) =>
+          dynamicChartSegementDashed(
+            ctx,
+            18 // number of data elements
+          ),
+      },
+    },
+  ],
+};
+
+const comorbidControlledConfig = baseLineChartConfig();
+comorbidControlledConfig.data = comorbidControlledData;
+comorbidControlledConfig.options.scales.y.ticks.callback = (val) => {
+  return val + "%";
+};
+comorbidControlledConfig.options.plugins.tooltip.callbacks = {
+  label: percentageLabel,
+};
+const comorbidControlledCanvas = document.getElementById("comodrbidcontrolled");
+if (comorbidControlledCanvas) {
+  createChart(comorbidControlledCanvas, comorbidControlledConfig);
+}
