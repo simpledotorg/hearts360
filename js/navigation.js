82acce4feb795e5d4d0d5ebf713891cbbf11f211
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2024-11-28 17:36:45 +0000

    Search shows result block on text entry
    - Improved design for highlight colour

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,5 @@
+// List item toggle: open and collapse
+// Note: this also creates the nested list of items
 var toggler = document.getElementsByClassName("caret");
 var i;
 
@@ -7,3 +9,22 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 }
+
+// Show results when typing in the search input and hide list
+const searchInput = document.getElementById("region-search");
+const results = document.getElementById("search-results");
+const regionList = document.getElementById("myUL");
+
+// Add an event listener to the input field
+searchInput.addEventListener("input", () => {
+  if (searchInput.value.trim() !== "") {
+    // Show the results if input is not empty
+    results.classList.remove("hidden");
+    regionList.classList.add("hidden");
+  } else {
+    // Hide the results if input is empty
+    results.classList.add("hidden");
+    regionList.classList.remove("hidden");
+  }
+});
+
